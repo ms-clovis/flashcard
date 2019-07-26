ccf6ce93a26b0986fe3cac96b919067f5faf2693
mike <ms.clovis@verizon.net>
2019-07-26 19:58:48 -0400

    Additional testing added

--- session/session_test.go
+++ session/session_test.go
@@ -45,9 +45,52 @@ func ExampleIsCorrectPassword() {
 }
 
 func TestCleanSessions(t *testing.T) {
+	time.Now()
 	theSession := Session{
 		UserName: "test",
 		LastUsed: time.Now().Add(-2 * time.Hour),
 	}
 	SessionMap["12345"] = theSession
+	CleanSessions()
+	if len(SessionMap) != 0 {
+		t.Fatal("Did not Clean the session")
+	}
 }
+
+func ExampleCleanSessions() {
+	time.Now()
+	theSession := Session{
+		UserName: "test",
+		LastUsed: time.Now().Add(-2 * time.Hour),
+	}
+	SessionMap["12345"] = theSession
+	CleanSessions()
+	_, ok := SessionMap["12345"]
+	fmt.Println(ok)
+	//Output:
+	//false
+}
+
+func TestUserExists(t *testing.T) {
+	user := User{UserName: "mike"}
+	UserMap["mike"] = user
+	_, result := UserExists("mike")
+	if result != true {
+		t.Fatal("User not found")
+	}
+}
+
+//func TestCreateUser(t *testing.T) {
+//
+//}
+
+//func TestCreateSession(t *testing.T) {
+//
+//}
+
+//func TestSetUserRole(t *testing.T) {
+//	url.Values["role"]= "Admin"
+//	req := & http.Request{
+//		PostForm:
+//	}
+//}
